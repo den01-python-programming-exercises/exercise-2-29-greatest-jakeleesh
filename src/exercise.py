856ba1227881e2772edf9c1cae532eb7d0c4ffef
Jake Lee <jake.lee@fdmgroup.com>
2021-09-23 18:32:11 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+    num1 = int(input())
+    num2 = int(input())
+    num3 = int(input())
+    answer = greatest(num1, num2, num3)
+    print("Greatest: " + str(answer))
+
+def greatest(num1, num2, num3):
+    if (num1 > num2 and num1 > num3):
+        return num1
+    elif (num2 > num1 and num2 > num3):
+        return num2
+    elif (num3 > num1 and num3 > num2):
+        return num3
 
 if __name__ == '__main__':
     main()
